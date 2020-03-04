e942c73ff8faceeb835449e158d5a8df748a2189
Sean W. Lawrence <sean.w.lawrence+programming@gmail.com>
2020-03-04 13:37:03 -0800

    Change build folder to app/views/application/react/build

--- packages/react-scripts/config/paths.js
+++ packages/react-scripts/config/paths.js
@@ -84,7 +84,7 @@ module.exports = {
   appPath: resolveApp('.'),
   // UPDATED
   // Move build folder to rails app views/application
-  appBuild: resolveApp('../app/views/application/react'),
+  appBuild: resolveApp('../app/views/application/react/build'),
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
   appIndexJs: resolveModule(resolveApp, 'src/index'),
