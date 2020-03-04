1a9c9d3ee99c941328e3b5ab11e1d401e8c3ffab
Sean W. Lawrence <sean.w.lawrence+programming@gmail.com>
2020-03-04 12:29:39 -0800

    Fix babel-preset-react-app-import

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -392,9 +392,7 @@ module.exports = function(webpackEnv) {
                 // @remove-on-eject-begin
                 babelrc: false,
                 configFile: false,
-                presets: [
-                  require.resolve('../../babel-preset-makeschool-react-app'),
-                ],
+                presets: [require.resolve('../../babel-preset-react-app')],
                 // Make sure we have a unique cache identifier, erring on the
                 // side of caution.
                 // We remove this when the user ejects because the default
