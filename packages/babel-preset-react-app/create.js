b405c811e834275b457909bcd30e4f813df47251
Sean W. Lawrence <sean.w.lawrence+programming@gmail.com>
2020-03-04 11:40:43 -0800

    Remove react-router-optimize babel  plugin

--- packages/babel-preset-react-app/create.js
+++ packages/babel-preset-react-app/create.js
@@ -118,11 +118,6 @@ module.exports = function(api, opts, env) {
       // Only during production
       isEnvProduction && 'lodash',
 
-      // UPDATED
-      // Convertoptimize React Router imports
-      // Only during production
-      isEnvProduction && 'transform-react-router-optimize',
-
       // Strip flow types before any other transform, emulating the behavior
       // order as-if the browser supported all of the succeeding features
       // https://github.com/facebook/create-react-app/pull/5182
